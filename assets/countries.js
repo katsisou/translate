1cf0542014b8b2aa34772d10e69a1272d2fbff86
katsisou <katrina.sisourath@gmail.com>
2023-10-03 10:43:21 +1100

    it's working
    Tried another input of the api key and it is working. It is able to translate any word of the selected language chosen.

---
+++ assets/countries.js
@@ -0,0 +1,18 @@
+const countries = {
+    "en-GB": "English",
+    "es-ES": "Spanish",
+    "ma-MA": "Mandarin",
+    "fr-FR": "French",
+    "ko-KR": "Korean",
+    "de-DE": "German",
+    "it-IT": "Italian",
+    "nl-NL": "Dutch",
+    "he-IL": "Hebrew",
+    "sv-SE": "Swedish",
+    "fi-FI": "Finnish",
+    "da-DK": "Danish",
+    "hi-IN": "Hindi",
+    "th-TH": "Thai",
+    "ja-JP": "Japanese",
+    "pt-PT": "Portuguese",
+}
